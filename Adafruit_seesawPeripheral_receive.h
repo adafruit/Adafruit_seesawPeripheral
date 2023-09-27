2d118728128b1fb927ded7932f09024d939cfed3
caternuson <caternuson@gmail.com>
2023-09-27 10:13:47 -0700

    add enc set pos

--- Adafruit_seesawPeripheral_receive.h
+++ Adafruit_seesawPeripheral_receive.h
@@ -294,6 +294,21 @@ void receiveEvent(int howMany) {
         }
       }
     }
+    else if ((module_cmd & 0xF0) ==  SEESAW_ENCODER_POSITION) {
+      if (howMany == 6) {
+      encoder_num = module_cmd & 0x0F;
+        if (encoder_num < CONFIG_NUM_ENCODERS){
+          uint32_t value = i2c_buffer[2];
+          value <<= 8;
+          value |= i2c_buffer[3];
+          value <<= 8;
+          value |= i2c_buffer[4];
+          value <<=8;
+          value |= i2c_buffer[5];
+          g_enc_value[encoder_num] = value;
+        }
+      }
+    }
   }
 #endif
 
